af1ea3216642602875979a3ec029ca9907709e40
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-05-16 17:34:25 +0800

    Update Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
             rel="noreferrer"
           >
             <FaTwitter style={{ display: "inline" }} />
-            @Chernestardreamer
+            @Cherneststardreamer
           </a>
         </GridItem>
         <GridItem justifySelf="end">
